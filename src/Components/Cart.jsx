beb8dd6464d266e1fade65b94e58a127657b48cd
IshikaChaudhary2 <ic37776313@gmail.com>
2024-12-28 19:34:19 +0530

    Update Cart.jsx

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,8 +3,16 @@ import React from 'react';
 
 const Cart = () => {
   const cartItems = [
-    { id: 1, name: 'Pizza', quantity: 2, price: 200 },
-    { id: 2, name: 'Burger', quantity: 1, price: 150 },
+    { id: 1, name: 'Pizzas', quantity: 10, price: 200-800 },
+    { id: 2, name: 'Burgers', quantity: 10, price: 250-450 },
+    { id: 3, name: 'Ice Cream', quantity: 10, price: 50-649 },
+    { id: 4, name: 'Chinese', quantity: 10, price: 450-800 },
+    { id: 5, name: 'Biryani', quantity: 10, price: 170-499 },
+    { id: 6, name: 'Cold Drinks', quantity: 10, price: 40-100 },
+    { id: 7, name: 'Momos', quantity: 10, price: 50-299 },
+    { id: 8, name: 'Desserts', quantity: 10, price: 80-899 },
+    { id: 9, name: 'Kebabs', quantity: 10, price: 149-400 },
+    { id: 10, name: 'Continental Food', quantity: 10, price: 79-650 },
   ];
 
   const totalPrice = cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0);

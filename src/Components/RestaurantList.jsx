821f9ae215bba7ba6ea3a222abde600353aa7684
IshikaChaudhary2 <ic37776313@gmail.com>
2025-01-18 21:13:17 +0530

    Update RestaurantList.jsx

--- src/Components/RestaurantList.jsx
+++ src/Components/RestaurantList.jsx
@@ -92,63 +92,63 @@ const RestaurantList = () => {
           name: 'Mushroom Swiss Burger',
           description: 'A beef patty topped with sautéed mushrooms and Swiss cheese. ',
           price: 380 ,
-          image: ""
+          image: "https://media.istockphoto.com/id/539659420/photo/mushroom-swiss-burger.jpg?s=612x612&w=0&k=20&c=jMNa1F6VJmUGvDafmgumvBqtWvFhyIPdgmvcxGKHWiM="
    },
         {
          id:2,
           name: 'Double Decker Burger',
           description: 'Two beef patties with cheese, lettuce, and special sauce stacked high.',
           price: 400,
-          image: ""
+          image: "https://www.shutterstock.com/image-photo/spectacular-doubledecker-burger-crispy-beef-600nw-2042006639.jpg"
    },  
         {
           id:2,
           name: 'Tandoori Chicken Burger',
           description: ' Tandoori-marinated chicken patty with mint yogurt sauce and onions.',
           price: 370,
-          image: ""
+          image: "https://img.freepik.com/premium-photo/tandoori-chicken-sandwich-burger-with-traditional-spices_641503-89129.jpg"
   },
         {
          id:2,
           name: 'Cheese Burst Burger',
           description: 'A beef patty topped with melted cheese, onions, and a special sauce. ',
           price: 350,
-          image: ""
+          image: "https://b.zmtcdn.com/data/dish_photos/f69/91af9363bf0dc5a730125e7bbb5d1f69.jpg"
   },
         {
           id:2,
-          name: 'Spicy Paneer Burger',
-          description: 'Grilled paneer marinated in spices, served with lettuce and mint chutney.',
+          name: 'Spicy Paneer Tikka Burger',
+          description: 'Grilled paneer marinated in Tikka,spices and served with lettuce and mint chutney.',
           price: 320,
-          image: ""  
+          image: "https://img.freepik.com/premium-photo/spicy-paneer-tikka-burger-white-background-paneer-tikka-image-photography_1020697-118615.jpg"  
   },
         {
           id:2,
           name: 'Veggie Delight Burger',
           description: 'A flavorful vegetable patty with fresh greens, tomatoes, and a tangy sauce.',
           price: 280,
-          image: ""
+          image: "https://png.pngtree.com/thumb_back/fh260/background/20241124/pngtree-juicy-beef-burger-delight-image_16636987.jpg"
   },
         {
           id:2,
           name: 'Fish Fillet Burger',
           description: ' Crispy fish fillet with tartar sauce, lettuce, and pickles.',
           price: 340,
-          image: ""
+          image: "https://plantbasednews.org/app/uploads/2024/11/plant-based-news-terris-fish-fillet-sandwich.jpg"
    }, 
         {
         id:2,
           name: 'Classic Chicken Burger',
           description: 'A juicy grilled chicken patty topped with lettuce, tomato, and mayo.',
           price: 300 ,
-          image: ""
+          image: "https://img.freepik.com/premium-photo/classic-chicken-burger_1272857-200324.jpg"
     },
         {
           id:2,
           name: 'Falafel Burger',
           description: 'A crispy falafel patty with tahini sauce, lettuce, and tomatoes.',
           price: 250,
-          image: ""
+          image: "https://www.eatingwell.com/thmb/ACveNSq0MByuKl_aLA_T-gaVlUY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5771127-b89da7c183cf406ca540364446837df8.jpg"
      },       
         
         {  
@@ -156,71 +156,71 @@ const RestaurantList = () => {
           name: 'Chocolate Fudge Brownie',
           description: 'Rich chocolate ice cream swirled with chunks of fudgy brownie, perfect for chocolate lovers.',
           price: 150,
-          image: ""
+          image: "https://www.shutterstock.com/image-photo/delicious-chocolate-brownie-on-polished-600nw-2502134497.jpg"
      },       
         {     
           id:3,
           name: 'Strawberry Swirl',
           description: 'Refreshing strawberry ice cream with a swirl of strawberry sauce, made from fresh strawberries.',
           price: 200 ,
-          image: ""
+          image: "https://wholefully.com/wp-content/uploads/2013/06/IMG_4924-735x1103.jpg"
     },      
         { 
           id:3,
           name: 'Mint Chocolate Chip',
           description: 'A cool mint ice cream loaded with chocolate chips, providing a refreshing and indulgent treat.',
           price: 250,
-          image: ""
+          image: "https://img.lovepik.com/photo/48013/3254.jpg_wh860.jpg"
     },
         {
           id:3,
           name: 'Mango Mania',
           description: 'Creamy mango ice cream made with ripe mangoes, capturing the essence of summer in every scoop.',
           price: 300,
-          image: ""
+          image: "https://images.pexels.com/photos/28525188/pexels-photo-28525188.jpeg?cs=srgb&dl=pexels-alejandro-aznar-155337093-28525188.jpg&fm=jpg"
    },        
          {
           id:3,
           name: 'Cookies and Cream'.
           description: 'A delightful blend of vanilla ice cream with crushed chocolate cookies, creating a crunchy texture.',
           price: 350,
-          image: ""
+          image: "https://richanddelish.com/wp-content/uploads/2022/05/cookies-and-cream-ice-cream-688x1024.jpg"
   },     
         {
           id:3,
           name: 'Pistachio Paradise',
           description: 'Smooth pistachio ice cream made with real pistachios, offering a nutty flavor and creamy consistency.',
           price: 400,
-          image: ""
+          image: "https://img.freepik.com/premium-photo/still-life-pistachio-ice-cream-bowl_659326-464.jpg"
   },   
 
   {
           id:3,
-          name: 'Caramel Crunch',
+          name: 'Caramel Cookie Crunch',
           description: 'Creamy caramel ice cream with crunchy caramel bits, delivering a sweet and satisfying experience.',
           price: 450,
-          image: ""
+          image: "https://5.imimg.com/data5/RJ/SA/JM/SELLER-4199470/caramel-cookie-crunch-ice-cream.jpg"
   },   
         {
           id:3,
           name: 'Tiramisu Twist',
           description: 'Coffee-flavored ice cream layered with mascarpone and cocoa, inspired by the classic Italian dessert.',
           price: 500,
-          image: ""
+          image: "https://img.freepik.com/premium-photo/tiramisu-with-twist-italian-dessert_198067-165400.jpg"
   },   
      {
           id:3,
           name: 'Rocky Road',
           description: 'A chocolate ice cream packed with marshmallows and nuts, providing a delightful mix of textures and flavors.',
           price: 600,
-          image: ""
+          image: "https://i0.wp.com/marbleandwhisk.com/wp-content/uploads/2020/12/Rocky-Road-3.jpg?fit=1200%2C1800&ssl=1"
   },   
       {
          id:3,
           name: 'Sorbet',
           description: 'fruit and water, typically dairy-free.',
           price: ,
-          image: ""
+          image: "https://t3.ftcdn.net/jpg/09/41/96/34/360_F_941963406_L4mePUAbJlTTxKgaoIjpnBHsrwfP1Czb.jpg"
   },   
      
         {
@@ -228,70 +228,70 @@ const RestaurantList = () => {
           name: 'Sweet and Sour Pork',
           description: 'This dish features tender pieces of pork coated in a bright orange-red sweet and sour sauce, often served with bell peppers and pineapple.',
           price: 800,
-          image: ""
+          image: "https://i0.wp.com/iankewks.com/wp-content/uploads/2023/10/IMG_0299.jpg?resize=800%2C1056&ssl=1"
   },   
       {
           id:4,
           name: 'Ma Po Tofu',
           description: 'A classic Sichuan dish made with soft tofu and minced meat (usually pork or beef) in a spicy sauce made from fermented black beans and chili paste.',
           price: 650,
-          image: ""
+          image: "https://www.seriouseats.com/thmb/CaR7btHrJgEO3OKZD1Z_795VmII=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2011__07__2021-02-12-Mapo-Tofu-MHOM-10-804db1211f1d47dbae505341d1e7b994.jpg"
   },   
      {
           id:4,
           name: 'Fried Rice',
           description: 'A versatile dish made from stir-fried rice with various ingredients such as eggs, vegetables, and meat or seafood, often enjoyed as a side or main dish.',
           price: 450,
-          image: ""
+          image: "https://www.allrecipes.com/thmb/2sI15_bQPb_LYm-SmQbzidHG_WA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/JF_21561_ShrimpFriedRice_4x3_13119-b29fd221132645f39db69b12a2525998.jpg"
   },   
        {
           id:4,
           name: 'Dim Sum',
           description: 'A collection of small dishes, including dumplings, buns, and pastries, often enjoyed with tea, perfect for sharing among friends and family.',
           price: 650,
-          image: ""
+          image: "https://thumbs.dreamstime.com/b/dim-sum-2578659.jpg"
   },   
        {
           id:4,
           name: 'Dumplings',
           description: 'These consist of minced meat and vegetables wrapped in thin dough, which can be boiled, steamed, or fried, making them a popular snack or appetizer.',
           price: 450,
-          image: ""
+          image: "https://media.istockphoto.com/id/1358463108/photo/asian-dumplings-with-soy-sauce.jpg?s=612x612&w=0&k=20&c=zubrIDmdrc3J76vJaBapB1miW-aUeZ05YpOON4u4N0g="
   },   
        {
           id:4,
           name: 'Twice-Cooked Pork',
           description: 'A Sichuan specialty where pork is first boiled, then stir-fried with a variety of spices and vegetables, resulting in a flavorful and tender dish.',
           price: 555,
-          image: ""
+          image: "https://www.beyondkimchee.com/wp-content/uploads/2022/11/Twice-Cooked-Pork-10.jpg"
   },   
        {
           id:4,
           name: 'Kung Pao Chicken',
           description: 'A spicy stir-fry featuring chicken, peanuts, and vegetables, seasoned with soy sauce and chili peppers.',
           price: 480,
-          image: ""
+          image: "https://www.seriouseats.com/thmb/BDJ9J1TKURp-WzODtAmxbrdg_Zg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2010__06__2021-02-12-Real-Deal-Kung-Pao-Chicken-MHOM-21-d8de1c4b127a4dc7b6d9e43e4b18a9e8.jpg"
   },   
        {
           id:4,
           name: 'Spicy Seafood Noodle Soup',
           description: 'A spicy noodle soup that features a rich broth made with seafood, vegetables, and chili oil. The dish is known for its bold flavors and is often served with a variety of seafood, including shrimp, squid, and clams.',
           price: 550,
-          image: ""
+          image: "https://www.koreanbapsang.com/wp-content/uploads/2011/04/DSC_0066-e1539057703301.jpg"
   },   
        {
           id:4,
           name: 'Black Bean Noodles',
           description: 'consisting of wheat noodles topped with a savory black bean sauce made from fermented black soybeans, diced pork, and vegetables. It’s often garnished with cucumber slices for a refreshing crunch.',
           price: 560,
-          image: ""
+          image: "https://www.curiousnut.com/wp-content/uploads/2015/06/Korean-Black-Bean-Noodles-New1-T.jpg"
   },   
        {
           id:4,
-          name: 'Tom Jap Chai',
+          name: 'Tom Jab Chai',
           description: 'This is a Chinese-inspired vegetable soup made by slow boiling leafy greens like cabbage and mustard greens with pork or chicken for a rich, meaty flavor. It is not spicy but is packed with umami from the long cooking process, making it a comforting dish.',
           price: 300,
-          image: ""
+          image: "https://as1.ftcdn.net/v2/jpg/02/48/99/12/1000_F_248991261_6Cr80mKmbKDNFA0eqQ0ouC9JaSCraKgX.jpg"
   },   
       
         {
@@ -299,113 +299,113 @@ const RestaurantList = () => {
           name: 'Hyderabadi Chicken Dum Biryani',
           description: 'Marinated chicken layered with fragrant basmati rice, slow-cooked to perfection. ',
           price: 250,
-          image: ""
+          image: "https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg"
   },   
        {
           id:5,
           name: 'Mutton Biryani',
           description: 'Tender mutton pieces cooked with aromatic spices and basmati rice, offering a rich and savory experience.',
           price: 450,
-          image: ""
+          image: "https://png.pngtree.com/thumb_back/fh260/background/20240328/pngtree-mutton-biryani-meal-in-a-plate-on-table-image_15645442.jpg"
   },   
        {
           id:5,
           name: 'Prawn Biryani',
           description: 'A coastal delight featuring succulent prawns cooked with fragrant rice and spices, offering a taste of the sea.',
           price: 499,
-          image: ""
+          image: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/12/Prawn-Biryani-Recipe.jpg"
   },   
        {
           id:5,
           name: 'Hyderabadi Chicken Dum Biryani',
           description: 'Marinated chicken layered with fragrant basmati rice, slow-cooked to perfection.',
           price: 250,
-          image: ""
+          image: "https://hinzcooking.com/wp-content/uploads/2019/08/Hyderabadi-Chicken-Dum-Biryani-Recipe-2-1.jpg"
   },   
        {
           id:5,
-          name: 'Persian Biryani',
+          name: 'Persian Lamb Biryani',
           description: 'A rich and aromatic dish made with saffron-infused rice, tender meat, and a blend of spices, often served with yogurt.',
           price: 450,
-          image: ""
+          image: "https://img.freepik.com/premium-photo/persian-lamb-biryani-with-barberries_1170794-58067.jpg"
   },   
        {
           id:5,
           name: 'Mutton Kacchi Biryani',
           description: ' Raw marinated mutton layered with partially cooked rice, slow-cooked to allow the flavors to meld beautifully.',
           price: 480,
-          image: ""
+          image: "https://static.wixstatic.com/media/7af7af_a0847a23f0584fbd8be11c99bb08d3ad~mv2.jpg/v1/fill/w_568,h_320,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/7af7af_a0847a23f0584fbd8be11c99bb08d3ad~mv2.jpg"
   },   
        {
           id:5,
           name: 'Chicken Biryani',
           description: 'Juicy chicken pieces cooked with basmati rice and a mix of spices, creating a comforting and hearty meal.',
           price: 220,
-          image: ""
+          image: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg"
   },   
        {
           id:5,
           name: 'Ayda Persian Kitchen Biryani',
           description: 'a blend of aromatic spices and is often served with a side of tangy raita.',
           price: 280,
-          image: ""
+          image: "https://b.zmtcdn.com/data/dish_photos/415/7c4411c1df981e268f4b46792aa64415.jpg"
   },   
        {
           id:5,
           name: 'Paneer Biryani ',
           description: 'A rich and creamy biryani made with paneer (Indian cottage cheese) cubes, basmati rice, and a mix of spices,by a side of yogurt or raita. ',
           price: 250,
-          image: ""
+          image: "https://ministryofcurry.com/wp-content/uploads/2023/10/paneer-biryani_-9.jpg"
   },   
        {
           id:5,
           name: 'Kuwaiti Biryani',
           description: 'A fragrant rice dish with marinated chicken or lamb, often garnished with nuts and raisins for a touch of sweetness.',
           price: 400,
-          image: ""
+          image: "https://www.finmail.com/wp-content/uploads/2021/06/Hyderabadi-chicken-Biryani-1.jpg"
   },   
      
         {
           id:6,
           name: 'Appy Fizz',
           description: 'A sparkling apple drink that combines the taste of apples with a fizzy twist.',
-          price: 50,
-          image: ""
+          price: 40,
+          image: "https://m.media-amazon.com/images/I/71DJ7ngZGKL._AC_UF1000,1000_QL80_.jpg"
   },   
         {
           id:6,
           name: 'Mountain Dew ',
           description: 'A citrus-flavored soft drink that is energizing and refreshing, perfect for outdoor activities.',
-          price: 80,
-          image: ""
+          price: 40,
+          image: "https://frugivore-bucket.s3.amazonaws.com/media/package/img_one/2020-10-17/Mountain_Dew__Soft_Drink_-_Mountain_Dew_Bottle__600_Ml_.jpg"
   },   
        {
           id:6,
           name: 'Coca-Cola',
           description: 'A classic carbonated soft drink with a refreshing cola flavor.',
-          price: 40,
-          image: ""
+          price: 50,
+          image: "https://images.pexels.com/photos/12960150/pexels-photo-12960150.jpeg?cs=srgb&dl=pexels-valeriia-harbuz-2161259-12960150.jpg&fm=jpg"
   },   
        {
           id:6,
           name: 'Sprite',
           description: 'A lemon-lime flavored soft drink that is crisp and refreshing, ideal for hot days.',
           price: 55,
-          image: ""
+          image: "https://thumbs.dreamstime.com/b/aluminium-can-sprite-isolated-over-white-background-119173810.jpg"
   },   
        {
           id:6,
           name: 'Red Bull Energy Drink',
           description: 'A popular energy drink known for its unique blend of caffeine, taurine, and B vitamins. It provides a quick energy boost and enhances alertness and concentration. The taste is slightly sweet with a hint of citrus.',
           price: 100,
-          image: ""
+          image: "https://vinepair.com/wp-content/uploads/2015/09/red-bull-card-375x450.jpg"
   },   
        {
           id:6,
           name: 'Monster Energy Drink',
           description: 'A carbonated energy drink that contains caffeine, ginseng, and B vitamins, designed to boost energy and enhance performance.',
           price: 110,
-          image: ""
+          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Monster_Energy_drink_%28cropped%29.jpg/1366px-Monster_Energy_drink_%28cropped%29.jpg"
   },   
        {
           id:6,
